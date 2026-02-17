87fa36461301c1590c1cdab86c5b43b1b13baf06
Arnau Pelechano García <arnaupg@kth.se>
2026-02-17 13:52:58 +0100

    feat: Assign ids to branches in matches function (#22)
    closes #22

--- mockito-core/src/test/java/org/CoverageTest.java
+++ mockito-core/src/test/java/org/CoverageTest.java
@@ -4,20 +4,31 @@
  */
 package org;
 
+import static org.junit.Assert.assertTrue;
+
 import java.nio.file.Path;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    org.mockitousage.matchers.MatchersTest.class
+})
 
 public class CoverageTest {
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeClass
+    public static void beforeAll() {
         // CoverageRecorder.register(101, 102, 103, 104, 105, 106);
+        CoverageRecorder.register(501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511);
     }
 
-    @AfterAll
-    static void afterAll() throws Exception {
+    @AfterClass
+    public static void afterAll() throws Exception {
         CoverageRecorder.dump(Path.of("build/reports/manual-coverage.txt"));
     }
 }
