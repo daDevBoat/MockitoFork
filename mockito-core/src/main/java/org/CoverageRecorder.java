ff57751306c2bbf1941d3203d5a9c27c05ea27f2
djonte <90456387+djonte@users.noreply.github.com>
2026-02-18 15:45:44 +0100

    feat: add branch coverage for processAnnotationForMock function (#57) #39
    closes #39

--- mockito-core/src/main/java/org/CoverageRecorder.java
+++ mockito-core/src/main/java/org/CoverageRecorder.java
@@ -8,9 +8,13 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class CoverageRecorder {
 
@@ -36,15 +40,26 @@ public class CoverageRecorder {
         TreeSet<Integer> missing = new TreeSet<>(allSorted);
         missing.removeAll(hitSorted);
 
-        double percent = allSorted.isEmpty() ? 0.0 : (100.0 * hitSorted.size() / allSorted.size());
+        HashMap<Character, List<Integer>> prefixCounter = new HashMap<>();
+        HashMap<Character, Integer> totalCounter = new HashMap<>();
+        for (int p : allSorted) {
+            char firstDigit = String.valueOf(p).charAt(0);
+            totalCounter.put(firstDigit, totalCounter.getOrDefault(firstDigit, 0) + 1);
+            if (hitSorted.contains(p)) {
+                prefixCounter.computeIfAbsent(firstDigit, k -> new ArrayList<>()).add(p);
+            }
+        }
 
-        String report =
-                "Manual branch coverage\n"
+        double totalPercent =
+                allSorted.isEmpty() ? 0.0 : (100.0 * hitSorted.size() / allSorted.size());
+
+        String totalReport =
+                "Total branch coverage\n"
                         + "Covered: "
                         + hitSorted.size()
                         + "/"
                         + allSorted.size()
-                        + String.format(" (%.2f%%)%n", percent)
+                        + String.format(" (%.2f%%)%n", totalPercent)
                         + "ALL IDs: "
                         + allSorted
                         + "\n"
@@ -54,9 +69,40 @@ public class CoverageRecorder {
                         + "MISS IDs: "
                         + missing
                         + "\n";
+        System.out.println(totalReport);
+
+        List<String> listOfReports = new ArrayList<>();
+        listOfReports.add(totalReport);
+
+        for (char prefix : new TreeSet<>(totalCounter.keySet())) {
+            System.out.println("------------------------------------");
+            System.out.println("HITS FOR PREFIX: " + prefix);
+
+            List<Integer> hitsIDS = prefixCounter.getOrDefault(prefix, List.of());
+            int covered = hitsIDS.size();
+            int total = totalCounter.get(prefix);
+
+            String hits = hitsIDS.stream().map(String::valueOf).collect(Collectors.joining(","));
+            double percent = (covered * 100.0 / total);
+            String report =
+                    "HITS FOR PREFIX: "
+                            + prefix
+                            + "\n"
+                            + "Manual branch coverage\n"
+                            + "Covered: "
+                            + covered
+                            + "/"
+                            + total
+                            + String.format(" (%.2f%%)%n", percent)
+                            + "HIT IDs: "
+                            + hits
+                            + "\n";
+            System.out.println(report);
+            listOfReports.add(report);
+        }
 
         Files.createDirectories(output.getParent());
-        Files.write(output, report.getBytes(StandardCharsets.UTF_8));
-        System.out.println(report); // also print to console
+        String allReports = String.join("\n-------------------------------\n", listOfReports);
+        Files.write(output, allReports.getBytes(StandardCharsets.UTF_8));
     }
 }
--- mockito-core/src/main/java/org/mockito/internal/configuration/MockAnnotationProcessor.java
+++ mockito-core/src/main/java/org/mockito/internal/configuration/MockAnnotationProcessor.java
@@ -4,18 +4,18 @@
  */
 package org.mockito.internal.configuration;
 
-import static org.mockito.internal.util.StringUtil.join;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import org.CoverageRecorder;
 import org.mockito.Mock;
 import org.mockito.MockSettings;
 import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.exceptions.base.MockitoException;
+import static org.mockito.internal.util.StringUtil.join;
 import org.mockito.internal.util.Supplier;
 import org.mockito.quality.Strictness;
 
@@ -23,6 +23,7 @@ import org.mockito.quality.Strictness;
  * Instantiates a mock on a field annotated by {@link Mock}
  */
 public class MockAnnotationProcessor implements FieldAnnotationProcessor<Mock> {
+
     @Override
     public Object process(Mock annotation, Field field) {
         return processAnnotationForMock(
@@ -32,32 +33,56 @@ public class MockAnnotationProcessor implements FieldAnnotationProcessor<Mock> {
     @SuppressWarnings("deprecation")
     public static Object processAnnotationForMock(
             Mock annotation, Class<?> type, Supplier<Type> genericType, String name) {
+
         MockSettings mockSettings = Mockito.withSettings();
         if (annotation.extraInterfaces().length > 0) { // never null
+            CoverageRecorder.mark(201);
             mockSettings.extraInterfaces(annotation.extraInterfaces());
+        } else {
+            CoverageRecorder.mark(202);
         }
         if ("".equals(annotation.name())) {
+            CoverageRecorder.mark(203);
             mockSettings.name(name);
         } else {
+            CoverageRecorder.mark(204);
             mockSettings.name(annotation.name());
         }
         if (annotation.serializable()) {
+            CoverageRecorder.mark(205);
             mockSettings.serializable();
+        } else {
+            CoverageRecorder.mark(206);
         }
         if (annotation.stubOnly()) {
+            CoverageRecorder.mark(207);
             mockSettings.stubOnly();
+        } else {
+            CoverageRecorder.mark(208);
         }
         if (annotation.lenient()) {
+            CoverageRecorder.mark(209);
             mockSettings.lenient();
+        } else {
+            CoverageRecorder.mark(210);
         }
         if (annotation.strictness() != Mock.Strictness.TEST_LEVEL_DEFAULT) {
+            CoverageRecorder.mark(211);
             mockSettings.strictness(Strictness.valueOf(annotation.strictness().toString()));
+        } else {
+            CoverageRecorder.mark(212);
         }
         if (!annotation.mockMaker().isEmpty()) {
+            CoverageRecorder.mark(213);
             mockSettings.mockMaker(annotation.mockMaker());
+        } else {
+            CoverageRecorder.mark(214);
         }
         if (annotation.withoutAnnotations()) {
+            CoverageRecorder.mark(215);
             mockSettings.withoutAnnotations();
+        } else {
+            CoverageRecorder.mark(216);
         }
 
         mockSettings.genericTypeToMock(genericType.get());
@@ -66,16 +91,19 @@ public class MockAnnotationProcessor implements FieldAnnotationProcessor<Mock> {
         mockSettings.defaultAnswer(annotation.answer());
 
         if (type == MockedStatic.class) {
+            CoverageRecorder.mark(217);
             return Mockito.mockStatic(
                     inferParameterizedType(
                             genericType.get(), name, MockedStatic.class.getSimpleName()),
                     mockSettings);
         } else if (type == MockedConstruction.class) {
+            CoverageRecorder.mark(218);
             return Mockito.mockConstruction(
                     inferParameterizedType(
                             genericType.get(), name, MockedConstruction.class.getSimpleName()),
                     mockSettings);
         } else {
+            CoverageRecorder.mark(219);
             return Mockito.mock(type, mockSettings);
         }
     }
